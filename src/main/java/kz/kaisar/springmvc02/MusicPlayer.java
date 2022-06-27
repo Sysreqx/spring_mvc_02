b29dc297b927fc6713fbd82e93c9a2ae80b08e86
Kaisar Yerdenbekov <sysreqx@gmail.com>
2022-06-27 15:14:39 +0600

    7. Dependency injection. Dependency Injection (DI). Part 2.

--- src/main/java/kz/kaisar/springmvc02/MusicPlayer.java
+++ src/main/java/kz/kaisar/springmvc02/MusicPlayer.java
@@ -4,8 +4,39 @@ public class MusicPlayer {
 
     private Music music;
 
+    private String name;
+
+    private int volume;
+
     public MusicPlayer(Music music) {
         this.music = music;
+    }
+
+    public MusicPlayer() {
+    }
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 
     public void playMusic() {
